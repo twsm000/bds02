fc7bfefa675670269c59fe2d20d05a267f3ecfd2
Thomas M <twsm000@gmail.com>
2021-06-09 12:20:21 -0300

    Add CIty and Event controllers

---
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -0,0 +1,31 @@
+package com.devsuperior.bds02.controllers;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+    @Autowired
+    private CityService cityService;
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        cityService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        return ResponseEntity.ok(cityService.findAll());
+    }
+}
